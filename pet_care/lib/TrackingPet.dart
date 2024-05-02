8e7a4fcad9291396d59841ab64d14805ae0b7478
Fuzail Raza <fuzailraza161@gmail.com>
2024-05-03 02:05:52 +0500

    Added Google Map API

--- pet_care/lib/TrackingPet.dart
+++ pet_care/lib/TrackingPet.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class trackingPet extends StatefulWidget {
   const trackingPet({super.key});
@@ -9,8 +11,21 @@ class trackingPet extends StatefulWidget {
 }
 
 class _trackingPetState extends State<trackingPet> {
-
+  final Completer<GoogleMapController> _controller =
+  Completer<GoogleMapController>();
   String dropdownvalue = 'Item 1';
+  LatLng _pGooglPlex = LatLng(-33.86, 151.20);
+  late GoogleMapController mapController;
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  }
+  static const CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
 
   // List of items in our dropdown menu
   var items = [
@@ -20,46 +35,50 @@ class _trackingPetState extends State<trackingPet> {
     'Item 4',
     'Item 5',
   ];
-  var values={
-    "Item 1":{
-      "Name":"Fuzail1",
-      "Email":"fuzailraza161@gmail.com"
-    },
-    "Item 2":{
-      "Name":"Fuzail2",
-      "Email":"fuzailraza161@gmail.com"
-    },
-    "Item 3":{
-      "Name":"Fuzail3",
-      "Email":"fuzailraza161@gmail.com"
-    },
-    "Item 4":{
-      "Name":"Fuzail4",
-      "Email":"fuzailraza161@gmail.com"
-    },
-    "Item 5":{
-      "Name":"Fuzail5",
-      "Email":"fuzailraza161@gmail.com"
-    }
+  var values = {
+    "Item 1": {"Name": "Fuzail1", "Email": "fuzailraza161@gmail.com","Lat":-33.86,"Long": 151.20},
+    "Item 2": {"Name": "Fuzail2", "Email": "fuzailraza161@gmail.com","Lat":-33.86,"Long": 151.20},
+    "Item 3": {"Name": "Fuzail3", "Email": "fuzailraza161@gmail.com","Lat":-33.86,"Long": 151.20},
+    "Item 4": {"Name": "Fuzail4", "Email": "fuzailraza161@gmail.com","Lat":-33.86,"Long": 151.20},
+    "Item 5": {"Name": "Fuzail5", "Email": "fuzailraza161@gmail.com","Lat":-33.86,"Long": 151.20}
   };
 
-  printValues(key){
+  printValues(key) {
     print("Name : ${values[key]?["Name"]} Email ${values[key]?["Name"]}");
-
   }
 
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = mapController;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Stack(
-        children:[
+    return Scaffold(
+      body: Container(
+
+        child: Stack(children: [
           Container(
-            height: 720,
+            // height: 720,
             color: Colors.blue,
+            child: GoogleMap(
+              myLocationButtonEnabled: true,
+              myLocationEnabled: true,
+              mapType: MapType.hybrid ,
+              onMapCreated: _onMapCreated,
+                initialCameraPosition:
+                    CameraPosition(target: _pGooglPlex, zoom: 13),
+              markers: {
+                const Marker(
+                  markerId: MarkerId('Sydney'),
+                  position: LatLng(-33.86, 151.20),
+                  infoWindow: InfoWindow(
+                    title: "First Map API",
+                    snippet: "Snipped of API",
+                  ),
 
-
-
+                )
+              },
+            ),
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
@@ -67,19 +86,15 @@ class _trackingPetState extends State<trackingPet> {
               // width: 400,
               height: 100,
               decoration: BoxDecoration(
-                  color: Colors.grey,
-                  borderRadius: BorderRadius.circular(10)
-              ),
+                  color: Colors.grey, borderRadius: BorderRadius.circular(10)),
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: DropdownButton(
-
                   dropdownColor: Colors.grey,
 
                   borderRadius: BorderRadius.circular(10),
 
                   isExpanded: true,
-
 
                   // Initial Value
                   value: dropdownvalue,
@@ -100,7 +115,6 @@ class _trackingPetState extends State<trackingPet> {
                         splashColor: Colors.yellow,
                         // trailing: Icon(Icons.keyboard_arrow_down_rounded),
                       ),
-
                     );
                   }).toList(),
                   // After selecting the desired option,it will
@@ -110,14 +124,16 @@ class _trackingPetState extends State<trackingPet> {
                       dropdownvalue = newValue!;
                     });
                     printValues(newValue);
-
+                    print("Map Controller Values ${mapController.mapId}");
                   },
                 ),
               ),
             ),
-          )
-      ]
+          ),
+        ]),
+
       ),
+
     );
   }
 }
